69af1ebfe583f4ae84c10f45924d9adad2dbd47d
UcezZ <ucezz@mail.ru>
2022-03-15 14:44:59 +0300

    hw task07

--- tasks/hw/task07/task07#27.py
+++ tasks/hw/task07/task07#27.py
@@ -1,5 +1,14 @@
+def bitSlice(hex, bitStart, bitEnd):
+    mask = 0
+    for i in range(bitStart, bitEnd):
+        mask |= 1 << i
+    return hex & mask
+
+
 def main(hex):
-    return (hex << 23) >> 7 | (hex >> 26) << 26
-
-
-print(hex(main(0xfe79050c)))
+    # var "t" for only PEP8 standard
+    t = bitSlice(hex, 26, 32)
+    t |= bitSlice(hex, 0, 10) << 16
+    t |= bitSlice(hex, 10, 15) << 1
+    t |= bitSlice(hex, 15, 26) >> 15
+    return t
