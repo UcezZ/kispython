f8bd18f062e52c69751f63b9eabcbee8f7bd6f23
UcezZ <ucezz@mail.ru>
2022-03-20 20:04:48 +0300

    subtask07 on task03 prototype

---
+++ tasks/task03/task07.py
@@ -0,0 +1,27 @@
+from random import random
+
+
+def sprite():
+    s = [
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0]
+    ]
+    for i in range(3):
+        for j in range(5):
+            s[j][i] = s[j][4-i] = True if random() > 0.5 else False
+    return s
+
+
+def printSprite(sprite):
+    for i in range(5):
+        for j in range(5):
+            print('\u25AE' if sprite[i][j] else ' ', end='')
+        print()
+
+
+for i in range(10):
+    printSprite(sprite())
+    print()
