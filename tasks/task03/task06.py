dbfadb06a819949d5a8c44e1d19e32d69ccb0281
UcezZ <ucezz@mail.ru>
2022-03-22 09:20:51 +0300

    subtask06 on task03

--- tasks/task03/task06.py
+++ tasks/task03/task06.py
@@ -1,11 +1,13 @@
 from datetime import datetime
 from traceback import extract_stack
+from os import chdir, path
 
 
 def run_with_log(func):
     try:
         func()
     except Exception as e:
+        chdir(path.dirname(path.abspath(__file__)))
         logFile = open(datetime.now().strftime('trace-%Y%m%d-%H%M%S.log'), 'a')
         logFile.write('Exception message list:\n')
         for a in e.args:
@@ -14,6 +16,7 @@ def run_with_log(func):
         for t in extract_stack():
             logFile.write('\tIn module \''+t.filename +
                           '\' at line #'+str(t.lineno)+'\n')
+        logFile.close()
 
 
 # print(datetime.now().strftime('crash-%Y%m%d-%H%M%S.log'))
