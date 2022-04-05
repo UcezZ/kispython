3c635c70768c2e5448275dd88a81d7f44e2bf070
UcezZ <ucezz@mail.ru>
2022-04-05 09:43:03 +0300

    hw task10

--- tasks/hw/task10/task10#27.py
+++ tasks/hw/task10/task10#27.py
@@ -1,10 +1,9 @@
-def rowEquals(row1, row2):
-    if len(row1) != len(row2):
-        return False
-    f = True
-    for i in range(len(row1)):
-        f &= row1[i] == row2[i]
-    return f
+def countOf(data, row):
+    c = 0
+    for r in data:
+        if r == row:
+            c += 1
+    return c
 
 
 def floatConvertCrutch(item):
@@ -25,9 +24,13 @@ def transpose(table):
 
 
 def main(data):
-    for i in range(len(data)-1):
-        if (rowEquals(data[i], data[i+1])):
+    i = 0
+    while i < len(data):
+        if countOf(data, data[i]) > 1:
             data.remove(data[i])
+            i = 0
+        else:
+            i += 1
     for i in range(len(data)):
         data[i][0] = floatConvertCrutch(data[i][0])
         data[i][1] = 'Выполнено' if data[i][1].lower(
@@ -41,3 +44,6 @@ def main(data):
 
 print(main([['0.8', 'false', '2002.11.27', 'kuvasak72[at]mail.ru'], ['0.8', 'false', '2001.03.17', 'zanko70[at]mail.ru'], ['0.5', 'false', '2003.08.12',
       'fazasic64[at]yahoo.com'], ['0.1', 'true', '2001.11.03', 'nozifskij76[at]yahoo.com'], ['0.1', 'true', '2001.11.03', 'nozifskij76[at]yahoo.com']]))
+
+print(main([['0.3', 'true', '2000.05.05', 'sotisak28[at]yahoo.com'], ['0.6', 'true', '2004.03.24', 'lolesidi88[at]gmail.com'], [
+      '0.7', 'false', '2003.04.05', 'zifov41[at]yahoo.com'], ['0.6', 'true', '2004.03.24', 'lolesidi88[at]gmail.com']]))
