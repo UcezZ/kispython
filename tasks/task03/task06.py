d34fb7be5ccaa5c96d9bd8a6e401a1a06378c6ca
UcezZ <ucezz@mail.ru>
2022-04-11 12:30:06 +0300

    subtask06 on task03

--- tasks/task03/task06.py
+++ tasks/task03/task06.py
@@ -10,16 +10,16 @@ def run_with_log(func):
         try:
             chdir(path.dirname(path.abspath(__file__)))
         except:
-            ()
-        logFile = open(datetime.now().strftime('trace-%Y%m%d-%H%M%S.log'), 'a')
-        logFile.write('Exception message list:\n')
-        for a in e.args:
-            logFile.write('\t'+a+'\n')
-        logFile.write('Stacktrace:\n')
-        for t in extract_stack():
-            logFile.write('\tIn module \''+t.filename +
-                          '\' at line #'+str(t.lineno)+'\n')
-        logFile.close()
+            logFile = open(datetime.now().strftime(
+                'trace-%Y%m%d-%H%M%S.log'), 'a')
+            logFile.write('Exception message list:\n')
+            for a in e.args:
+                logFile.write('\t'+a+'\n')
+            logFile.write('Stacktrace:\n')
+            for t in extract_stack():
+                logFile.write('\tIn module \''+t.filename +
+                              '\' at line #'+str(t.lineno)+'\n')
+            logFile.close()
 
 
 run_with_log(lambda: 0/0)
