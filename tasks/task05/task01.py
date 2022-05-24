b0a143974d183f514bf6419d45c41649b3f42a78
UcezZ <ucezz@mail.ru>
2022-05-24 09:38:00 +0300

    bucket sort

---
+++ tasks/task05/task01.py
@@ -0,0 +1,18 @@
+def bucketsort(arr, k):
+    counts = [0] * k
+    for x in arr:
+        counts[x] += 1
+
+    print(counts)
+
+    sorted_arr = []
+    for i, count in enumerate(counts):
+        sorted_arr.extend(count * [i])
+
+    return sorted_arr
+
+
+arr = [9, 4, 7, 2, 5, 3, 6, 1, 2]
+k = max(arr) + 1
+
+print(bucketsort(arr, k))
