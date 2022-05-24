ab7e58fb906b7af60ceee4cf593a935d394039c1
UcezZ <ucezz@mail.ru>
2022-05-24 10:26:57 +0300

    bucketsort

--- tasks/task05/task01.py
+++ tasks/task05/task01.py
@@ -2,9 +2,6 @@ def bucketsort(arr, k):
     counts = [0] * k
     for x in arr:
         counts[x] += 1
-
-    print(counts)
-
     sorted_arr = []
     for i, count in enumerate(counts):
         sorted_arr.extend(count * [i])
